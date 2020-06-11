fa6b52e7b258e6f5f0acf36056029b348ceee9ac
Heriberto Alcantara <elfeo.0103@gmail.com>
2020-06-11 01:05:20 -0400

    Adding Shared folder and ImageViewerPage

--- AntonyCelulares/Program.cs
+++ AntonyCelulares/Program.cs
@@ -1,23 +1,61 @@
+using AntonyCelulares.Data;
+using AntonyCelulares.Views.Account;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Configuration;
 using System.Windows.Forms;
 
 namespace AntonyCelulares
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+        /// 
+
+        
         [STAThread]
         static void Main()
         {
+            
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            ServiceCollection services = new ServiceCollection();
+
+            ConfigureServices(services);
+
+            using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+            {
+                var form1 = serviceProvider.GetRequiredService<Form1>();
+                Application.Run(form1);
+            }
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+
+            /*services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });*/
+            services.AddLogging(configure => configure.AddConsole());
+            #region Views
+            
+            services.AddScoped<Form1>();
+            //Account
+            services.AddScoped<ListaUsuarioPage>();
+            services.AddScoped<RegisterUsuarioPage>();
+            services.AddScoped<LoginPage>();
+
+
+
+            #endregion
         }
     }
 }
